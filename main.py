da95ebb402359171a464d33b200d5bf13392af6a
volter111 <krut.valeriy@gmail.com>
2021-11-01 14:49:26 -0700

    Notepad PyQt

---
+++ main.py
@@ -0,0 +1,37 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Блокнот")
+        self.setGeometry(500, 250, 600, 400)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Ну оооочень длинная надпись")
+        self.main_text.move(75, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(50, 150)
+        self.btn.setText('Кнопка')
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
+
+    def add_label(self):
+        print('Some console text')
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()
+
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
